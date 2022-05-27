46c2885a6881d94b5ee40e9e2ffc1b87e3512082
adarsh <201b358@juetguna.in>
2022-05-27 15:34:58 +0530

    commit

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -30,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
     int []performance={-1,-1,-1,-1,-1,-1}; //score of a game is updated in this array
     int []score={-1,-1,-1}; //score of each match is updated in this array. A total of three matches in a game
     String operators[]={"+","-","*","/"};
-    int correctButton=0; //which button will have the correct answer (tag of that button)
-    Random random=new Random(); //You will generate randdom alegebra questions
+     //which button will have the correct answer (tag of that button)
+    Random random=new Random();
+    int correctButton=0;//You will generate randdom alegebra questions
     TextView textView2;
     Button button1,button2,button3,button4;
     public void load(View view){
@@ -72,12 +73,46 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
-        //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
+        int operand2 = random.nextInt(10);
+        int correctButton= random.nextInt(4);
+        int correctAnswer =-100;
         String operator = operators[random.nextInt(4)];
+        if (operator.equals("+"))
+            correctAnswer= operand1+operand2;
+        else if (operator.equals("-"))
+            correctAnswer=operand1-operand2;
+        else if (operator.equals("/"))
+            correctAnswer=operand1/operand2;
+        else
+            correctAnswer=operand1*operand2;
+        //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
+        if (correctButton==0){
+            button1.setText(correctAnswer+"");
+            button2.setText(correctAnswer+1+"");
+            button3.setText(correctAnswer-1+"");
+            button4.setText(correctAnswer+2+"");
+        }
+        if (correctButton==1) {
+            button1.setText(correctAnswer+"");
+            button2.setText(correctAnswer+1+"");
+            button3.setText(correctAnswer-1+"");
+            button4.setText(correctAnswer+2+"");
+        }
+        if (correctButton==2) {
+            button1.setText(correctAnswer+"");
+            button2.setText(correctAnswer+1+"");
+            button3.setText(correctAnswer-1+"");
+            button4.setText(correctAnswer+2+"");
+        }
+        if (correctButton==3) {
+            button1.setText(correctAnswer+"");
+            button2.setText(correctAnswer+1+"");
+            button3.setText(correctAnswer-1+"");
+            button4.setText(correctAnswer+2+"");
+        }
         textView2.setText(operand1 + operator + operand2);
 
-      // Your code here, to diplay correct and incorrect options on the buttons
+        // Your code here, to diplay correct and incorrect options on the buttons
 
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
@@ -91,11 +126,14 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
-        int sum=0;
+
        // your code here
+        int sum=0;
+        for (int i=0;i<score.length;i++){
+            sum=sum+score[i];
+        }
         return sum;
     }
 
